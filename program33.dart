c9d91b0fdb5a97b8f9701fea31f798a34adf4eee
vivekt20 <vivekcherupoika20@gmail.com>
2024-07-19 14:24:22 +0530

    conditalstatements on 19/07/2024

---
+++ program33.dart
@@ -0,0 +1,19 @@
+void main(){
+String wheather="cloudy";
+switch(wheather){
+case "sunny":
+print("put the sunscreem");
+break;
+case "snowy":
+print("it is snow time");
+break;
+case "cloudy":
+print("bring umbrella");
+break;
+default:
+print("invalid");
+break;
+
+}
+
+}
